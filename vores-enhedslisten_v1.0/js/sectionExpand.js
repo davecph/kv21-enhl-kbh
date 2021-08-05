52e782e768039e56df07ac5d48c01ba9a9e1f451
davecph <design@ponr.dk>
2021-08-05 23:05:40 +0200

    same

--- vores-enhedslisten_v1.0/js/sectionExpand.js
+++ vores-enhedslisten_v1.0/js/sectionExpand.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
    console.log('ready!');
    $('#expandCandidates').click(function() {
-      console.log('clicked yup');
+      console.log('clicked yup!');
       if ($("#candidateGrid").hasClass("displayAll") !== true) {
          $("#candidateGrid").addClass("displayAll");
-      } else if ($("#candidateGrid").hasClass("displayAll") = true) {
+      } else 
+      if ($("#candidateGrid").hasClass("displayAll") == true) {
          console.log('here');
          $("#candidateGrid").removeClass("displayAll");
       }
