06306c81f31f13dd08bde51d4a349deae2366701
davecph <design@ponr.dk>
2021-08-05 22:59:04 +0200

    same

--- vores-enhedslisten_v1.0/js/sectionExpand.js
+++ vores-enhedslisten_v1.0/js/sectionExpand.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
    console.log('ready!');
-
    $('#expandCandidates').click(function() {
       console.log('clicked');
       if ($("#candidateGrid").hasClass("displayAll") !== true) {
-         
          $("#candidateGrid").addClass("displayAll");
       } else if ($("#candidateGrid").hasClass("displayAll") == true) {
-         
+         console.log('here');
          $("#candidateGrid").removeClass("displayAll");
       }
    });
-   
 });
