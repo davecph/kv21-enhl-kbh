182a2056f07953f56708fa600cfd4248061ac58f
davecph <design@ponr.dk>
2021-08-06 12:42:34 +0200

    same

--- vores-enhedslisten_v1.0/js/sectionExpand.js
+++ vores-enhedslisten_v1.0/js/sectionExpand.js
@@ -4,21 +4,14 @@ $(document).ready(function() {
       console.log('clicked yup!');
       if ($("#candidateGrid").hasClass("displayAll") !== true) {
          $("#candidateGrid").addClass("displayAll");
-      } else 
+      } else
       if ($("#candidateGrid").hasClass("displayAll") == true) {
          console.log('here');
          $("#candidateGrid").removeClass("displayAll");
       }
    });
-});
-
-
-$(document).ready(function() {
-   $(document).click(function(event){
+   $(document).click(function(event) {
       console.log('stuff');
-      $(event.target).closest(".clickable").each(function(){
-        // "this" is your "clickable" clicked
-        
-      });
-    });
+      
+   });
 });
