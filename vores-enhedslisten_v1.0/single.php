e16ee2a3a464061f4a35dfd2f35304b4253baf85
davecph <design@ponr.dk>
2021-08-07 14:33:19 +0200

    same

--- vores-enhedslisten_v1.0/single.php
+++ vores-enhedslisten_v1.0/single.php
@@ -61,8 +61,8 @@ get_header();
 
             
 				array(
-					'prev_text' => '<span class="nav-subtitle"><span class="icon_arrow-l_white_small"></span>'.get_the_post_thumbnail($prev_post->ID,'thumbnail').'<span class="nav-title">%title</span></span>',
-					'next_text' => '<span class="nav-subtitle">'.get_the_post_thumbnail($next_post->ID,'thumbnail').'<span class="nav-title">%title</span><span class="icon_arrow-r_white_small"></span></span>',
+					'prev_text' => '<span class="nav-subtitle"><span class="icon_arrow-l_white_medium"></span>'.get_the_post_thumbnail($prev_post->ID,'thumbnail').'<span class="nav-title">%title</span></span>',
+					'next_text' => '<span class="nav-subtitle">'.get_the_post_thumbnail($next_post->ID,'thumbnail').'<span class="nav-title">%title</span><span class="icon_arrow-r_white_medium"></span></span>',
 				)
 			);
          echo "<!-- 04 -->";
