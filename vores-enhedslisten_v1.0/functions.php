528200eff2de3f267250d34b2947b6c39d586874
davecph <design@ponr.dk>
2021-08-17 12:56:09 +0200

    same

--- vores-enhedslisten_v1.0/functions.php
+++ vores-enhedslisten_v1.0/functions.php
@@ -278,7 +278,7 @@ function frm_populate_posts($values, $field){
                            'key'	 	=> 'event_type',
                            'value'	  	=> 'Aktivist event',
                            'compare' 	=> 'IN',
-                        ),
+                        )
                      ),
                      'post_status' => array(
                         'publish', 'private'), 
