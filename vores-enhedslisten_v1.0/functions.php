8e323b6fe29824b6fe1157ecd68ec3c3b33dcf12
davecph <design@ponr.dk>
2021-08-25 16:50:45 +0200

    same

--- vores-enhedslisten_v1.0/functions.php
+++ vores-enhedslisten_v1.0/functions.php
@@ -251,7 +251,7 @@ function sortCandidateEventsByDate($args, $widgetControlsValues){
 	
    $currentPost =  get_the_title();
 
-   $variable = get_field('tag_candidates', 483 );
+   $variable = 'john';
    /* Sorts posts "kurser" by starting date  */
 	
     $args[category] = 'event';
