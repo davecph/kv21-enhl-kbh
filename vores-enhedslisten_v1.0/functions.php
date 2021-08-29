311f086f7976b5e1f547be59a320db6d2a7b70d0
davecph <design@ponr.dk>
2021-08-29 17:49:17 +0200

    same

--- vores-enhedslisten_v1.0/functions.php
+++ vores-enhedslisten_v1.0/functions.php
@@ -195,7 +195,7 @@ function my_ajax_pagination() {
 
    $query_vars = json_decode( stripslashes( $_POST['query_vars'] ), true );
    print_r($query_vars);
-   echo get_the_title($query_vars);
+   echo get_the_content($query_vars);
    die();
 }
 
