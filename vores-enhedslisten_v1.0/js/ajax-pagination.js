c9ccb543378851edcfc35c23e7396f5c7b5e17ca
davecph <design@ponr.dk>
2021-08-29 17:15:46 +0200

    same

--- vores-enhedslisten_v1.0/js/ajax-pagination.js
+++ vores-enhedslisten_v1.0/js/ajax-pagination.js
@@ -13,7 +13,7 @@
          data: {
 				action: 'ajax_pagination',
 				query_vars: ajaxpagination.query_vars,
-				page: page
+				page: $post_id
 			},
          success: function( result ) {
             console.log( result );
