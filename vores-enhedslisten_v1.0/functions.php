024d3558adc4e0565294c5f54bfe033c633c9704
davecph <design@ponr.dk>
2021-08-29 16:09:34 +0200

    same

--- vores-enhedslisten_v1.0/functions.php
+++ vores-enhedslisten_v1.0/functions.php
@@ -180,13 +180,7 @@ function test_scripts() {
          'ajaxurl' => admin_url( 'admin-ajax.php' )
       ));
 
-      add_action( 'wp_ajax_nopriv_ajax_pagination', 'my_ajax_pagination' );
-add_action( 'wp_ajax_ajax_pagination', 'my_ajax_pagination' );
-
-function my_ajax_pagination() {
-    echo get_bloginfo( 'title' );
-    die();
-}
+      
 
    /* / get post content with ajax*/
 
