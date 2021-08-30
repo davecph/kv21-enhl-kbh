5456baf715676cd8f70b633bb6aaff1b97d3195e
davecph <design@ponr.dk>
2021-08-30 11:24:08 +0200

    fix error

--- vores-enhedslisten_v1.0/template-parts/ajaxpop.php
+++ vores-enhedslisten_v1.0/template-parts/ajaxpop.php
@@ -1,5 +1,5 @@
 <div id="ajaxpop">
-   <div class="ajaxpop-container>">
+   <div class="ajaxpop-container">
       <div class="ajaxpop-close"></div>
       <div class="ajaxpop-content">
          <section id="coverImg">
