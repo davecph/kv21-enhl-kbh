21dc880010e5be92e8bc82a20b8d633bfd2be1b5
davecph <design@ponr.dk>
2021-08-30 15:59:17 +0200

    same

--- vores-enhedslisten_v1.0/template-parts/ajaxpop.php
+++ vores-enhedslisten_v1.0/template-parts/ajaxpop.php
@@ -1,8 +1,7 @@
 <div id="ajaxpop">
    <div class="ajaxpop-cover"></div>
-   <div class="ajaxpop-close icon_close_white_medium"></div>
    <div class="ajaxpop-container">
-      
+      <div class="ajaxpop-close icon_close_white_medium"></div>
       <div class="ajaxpop-content">
          <section id="coverImg">
          </section>
