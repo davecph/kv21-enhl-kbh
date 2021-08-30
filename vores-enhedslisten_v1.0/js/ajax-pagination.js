f1f4bc17ee359c8b5c6d4b7773a0dd688b41ed26
davecph <design@ponr.dk>
2021-08-30 11:36:15 +0200

    same

--- vores-enhedslisten_v1.0/js/ajax-pagination.js
+++ vores-enhedslisten_v1.0/js/ajax-pagination.js
@@ -1,37 +1,53 @@
-
-   (function($) {
-      $(document).on('click', '.eventSection a', function(event) {
-         event.preventDefault();
-         $post_id = $(this).attr('id').replace('post_item_', '');
-         console.log($post_id);
-         $.ajax({
-            url: ajaxpagination.ajaxurl,
-            type: 'post',
-            data: {
-               action: 'ajax_pagination',
-               query_vars: $post_id,
-            },
-            success: function(data) {
-               $stuff = JSON.parse(data);
-               //alert($stuff.event_start_time);
-               console.log($stuff);
-               $('#ajaxpop .ajaxpop-content').append($stuff.event_start_time);
-               $('#ajaxpop .ajaxpop-content').append($stuff.thumbnail_url);
-               $('#ajaxpop .ajaxpop-content').append($stuff.ID);
-            }
-         })
-
-         if ($("#ajaxpop").hasClass("show") !== true) {
-            $("#ajaxpop").addClass("show");
-            $("body").addClass("noScroll");
-         } else
-         if ($("#ajaxpop").hasClass("show") == true) {
-            console.log('here');
-            $("#ajaxpop").removeClass("show");
+(function($) {
+   $(document).on('click', '.eventSection a', function(event) {
+      event.preventDefault();
+      $post_id = $(this).attr('id').replace('post_item_', '');
+      console.log($post_id);
+      $.ajax({
+         url: ajaxpagination.ajaxurl,
+         type: 'post',
+         data: {
+            action: 'ajax_pagination',
+            query_vars: $post_id,
+         },
+         success: function(data) {
+            $stuff = JSON.parse(data);
+            //alert($stuff.event_start_time);
+            console.log($stuff);
+            $('#ajaxpop .ajaxpop-content').append($stuff.event_start_time);
+            $('#ajaxpop .ajaxpop-content').append($stuff.thumbnail_url);
+            $('#ajaxpop .ajaxpop-content').append($stuff.ID);
          }
-
-
-
       })
+      if ($("#ajaxpop").hasClass("show") !== true) {
+         $("#ajaxpop").addClass("show");
+         $("body").addClass("noScroll");
+      } else
+      if ($("#ajaxpop").hasClass("show") == true) {
+         console.log('here');
+         $("#ajaxpop").removeClass("show");
+      }
    })
+})
+(function($) {
+   $(document).on('click', '.ajaxpop-close', function(event) {
+      event.preventDefault();
+      if ($("#ajaxpop").hasClass("show") == true) {
+         console.log('here');
+         $("#ajaxpop").removeClass("show");
+         $("body").removeClass("noScroll");
+      }
+   })
+})
+
+(function($) {
+   $(document).on('click', '.ajaxpop-cover', function(event) {
+      event.preventDefault();
+      if ($("#ajaxpop").hasClass("show") == true) {
+         console.log('here');
+         $("#ajaxpop").removeClass("show");
+         $("body").removeClass("noScroll");
+      }
+   })
+})
 (jQuery);
