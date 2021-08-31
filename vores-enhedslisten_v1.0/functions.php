f3f486bf39c5a9a014ed7e8b99077f5bf9d9121d
davecph <design@ponr.dk>
2021-08-31 11:39:32 +0200

    same rm console log

--- vores-enhedslisten_v1.0/functions.php
+++ vores-enhedslisten_v1.0/functions.php
@@ -409,7 +409,6 @@ function frm_populate_posts($values, $field){
     $values['options'] = array(''); //remove this line if you are using a checkbox or radio button field
     $values['options'][''] = ''; //remove this line if you are using a checkbox or radio button field
 
-    console_log(print_r($posts).'hey');
     foreach($posts as $p){
       /* $date_now = date('YmdHis');
       $compare_time = get_field('event_start_time', $p->ID, false );
