166ab6a4cbe9c2ca5bcff69c07aa54be6b13916c
davecph <design@ponr.dk>
2021-08-31 17:01:01 +0200

    same

--- vores-enhedslisten_v1.0/js/sectionExpand.js
+++ vores-enhedslisten_v1.0/js/sectionExpand.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   console.log('ready!!');
+   console.log('ready!!!!');
    $('[id*="expand_btn_"]').click(function() {
       
       $item_id = $(this).attr('id');
