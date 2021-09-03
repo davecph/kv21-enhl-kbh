8082b52c029fe67fd010bdb33b08087027858a52
davecph <design@ponr.dk>
2021-09-03 14:46:17 +0200

    same

--- vores-enhedslisten_v1.0/header.php
+++ vores-enhedslisten_v1.0/header.php
@@ -18,7 +18,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="profile" href="https://gmpg.org/xfn/11"> <?php wp_head(); ?> 
 
-   <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri( ); ?>/style.css?v=<?php echo date("ymdhis"); ?>">
+   
    </link> 
 </head>
 
