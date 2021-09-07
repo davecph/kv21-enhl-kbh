07a1dcfa8e02ecb1835282e89ca6d1caed7ce939
davecph <design@ponr.dk>
2021-09-08 00:23:38 +0200

    same

--- vores-enhedslisten_v1.0/header.php
+++ vores-enhedslisten_v1.0/header.php
@@ -14,16 +14,14 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-   <meta charset="<?php bloginfo( 'charset' ); ?>">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="profile" href="https://gmpg.org/xfn/11"> 
-   <?php wp_head(); ?> 
-
-   
-   
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
 </head>
 <!-- Great FUCK -->
-<body <?php body_class(); ?>> <?php wp_body_open(); ?> <div id="page" class="site">
+<body <?php body_class(); ?>> 
+<?php wp_body_open(); ?> 
+<div id="page" class="site">
      
 
 
