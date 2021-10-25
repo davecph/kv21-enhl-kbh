716fce2e160fae2e427e37e9fb64552157bea810
davecph <design@ponr.dk>
2021-10-25 12:54:36 +0200

    same

--- vores-enhedslisten_v1.0/functions.php
+++ vores-enhedslisten_v1.0/functions.php
@@ -285,8 +285,8 @@ function shortcodes_init(){
 add_action('init', 'shortcodes_init');
 
 function SC_post_title_function( $post_id ){
-   echo 'heer';
-   get_the_title($post_id);
+   console_log($post_id) ;
+   //get_the_title($post_id);
 }
 
 /**
